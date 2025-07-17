3c5f6d0d6a327321fa59e64dcec896b32f367510
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2025-07-18 01:23:18 +0530

    username unique and verify code api completed

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,63 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "Account Verified Successfully",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification Code has expired.Please signup again for new code",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Verification Code is Incorrect.",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying User:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
